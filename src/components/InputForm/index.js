b64065f9425e9d1987a7b8e3782c73f23e65cc9a
Your name herecod3rcarl <your_email@example.comcod3rcarl@gmail.com>
2020-11-10 19:15:46 +0000

    working back-end

--- src/components/InputForm/index.js
+++ src/components/InputForm/index.js
@@ -9,66 +9,31 @@ function InputForm() {
   const [q4, setq4] = useState("");
   const [q5, setq5] = useState("");
 
-  function handleQ1Change(e) {
-    setq1(e.target.value);
-  }
-  function handleQ2Change(e) {
-    setq2(e.target.value);
-  }
-  function handleQ3Change(e) {
-    setq3(e.target.value);
-  }
-  function handleQ4Change(e) {
-    setq4(e.target.value);
-  }
-  function handleQ5Change(e) {
-    setq5(e.target.value);
-  }
-
-  function handleClick(e) {
+  async function handleClick(e) {
+    console.log("clicked");
     e.preventDefault();
+
     // POST request using fetch inside useEffect React hook
     const requestOptions = {
       method: "POST",
-      mode: 'no-cors',
+
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(q1),
+      body: JSON.stringify({ data: { q1, q2, q3, q4, q5 } }),
     };
-    fetch(`http://localhost:3000`, requestOptions)
-      .then((response) => response.json())
-      .then((data) => setq1(data.q1));
-    console.log(q1);
-    // empty dependency array means this effect will only run once (like componentDidMount in classes)
-
-    //   console.log(q1);
-    //   console.log(q2);
-    //   console.log(q3);
-    //   console.log(q4);
-    //   console.log(q5);
-    //
+    const response = await fetch(`http://localhost:3000`, requestOptions);
+    const data = await response.json();
+    setq1({ q1: data.q1 });
   }
-  // useEffect(() => {
-  //   async function addData() {
-  //     const response = await fetch(`http://localhost:3000`, {
-  //       method: "POST",
-  //       headers: { "Content-Type": "application/json" },
-  //       body: JSON.stringify(q1),
-  //     });
-  //     const data = await response.json();
-  //     console.log(data);
-  //   }
-  //   addData();
-  // }, []);
 
-  // useEffect(() => {
-  //   async function getQ1() {
-  //     const res = await fetch("http://localhost:3000");
-  //     const data = await res.json();
-  //     console.log(data.payload);
-  //     q1(data.payload);
-  //   }
-  //   getQ1();
-  // }, []);
+  useEffect(() => {
+    async function getQ1() {
+      const res = await fetch("http://localhost:3000/5");
+      const data = await res.json();
+      console.log(data.payload);
+      setq1(data.payload);
+    }
+    getQ1();
+  }, []);
 
   return (
     //function for onChange
@@ -78,7 +43,7 @@ function InputForm() {
         <Form.Group controlId="formq1">
           <Form.Label>Q1</Form.Label>
           <Form.Control
-            onChange={handleQ1Change}
+            onChange={(e) => setq1(e.target.value)}
             className="answer1"
             type="q1"
             placeholder="Enter answer here..."
@@ -88,7 +53,7 @@ function InputForm() {
         <Form.Group controlId="formq2">
           <Form.Label>Q2</Form.Label>
           <Form.Control
-            onChange={handleQ2Change}
+            onChange={(e) => setq2(e.target.value)}
             type="q2"
             placeholder="Enter answer here..."
           />
@@ -96,7 +61,7 @@ function InputForm() {
         <Form.Group controlId="formq3">
           <Form.Label>Q3</Form.Label>
           <Form.Control
-            onChange={handleQ3Change}
+            onChange={(e) => setq3(e.target.value)}
             type="q3"
             placeholder="Enter answer here..."
           />
@@ -104,7 +69,7 @@ function InputForm() {
         <Form.Group controlId="formq4">
           <Form.Label>Q4</Form.Label>
           <Form.Control
-            onChange={handleQ4Change}
+            onChange={(e) => setq4(e.target.value)}
             type="q4"
             placeholder="Enter answer here..."
           />
@@ -112,7 +77,7 @@ function InputForm() {
         <Form.Group controlId="formq5">
           <Form.Label>Q5</Form.Label>
           <Form.Control
-            onChange={handleQ5Change}
+            onChange={(e) => setq5(e.target.value)}
             type="q5"
             placeholder="Enter answer here..."
           />
