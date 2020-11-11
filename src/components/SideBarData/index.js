959b6332795e9ab2425c343fde0bc82189df40b8
FreshtaEbrahim <56118343+FreshtaEbrahim@users.noreply.github.com>
2020-11-11 09:50:38 +0000

    cleanup

--- src/components/SideBarData/index.js
+++ src/components/SideBarData/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
-import * as IoIcons from 'react-icons/io';
+import AiFillHome from 'react-icons/ai';
+
 
 export const SidebarData = [
   {
     title: 'Home',
     path: '/',
-    icon: <AiIcons.AiFillHome />,
+    icon: <AiFillHome />,
     class: 'nav-text'
   },
   {
