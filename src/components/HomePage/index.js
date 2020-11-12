e68cb65f52d6692e0766459089b98a2091ea705c
Jon Owens <jonowens84@gmail.com>
2020-11-12 16:48:33 +0000

    logo updated needs moving

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -2,19 +2,27 @@ import React from "react";
 import {Jumbotron} from 'react-bootstrap';
 import '../App/App.css';
 import stars from '../images/starryNight.jpeg';
+import logo from '../images/2.png';
 
 function HomePage() {
   return (
+      <div>
     <div className="page-intro">
      <Jumbotron style = {{backgroundImage:`url(${stars})`,backgroundSize:"cover", backgroundPosition:"center"}} >
-         {/* <img src = {stars}/> */}
-  <h1 style={{color:"white"}}>Homepage</h1>
-  <p style={{color:"white",fontSize:"24px"}}>
+    
+  <h1 style={{color:"white"}}>MENTOR DIARY</h1>
+ 
+</Jumbotron>
+</div>
+<div> 
+<p style={{color:"black",fontSize:"24px"}}>
   As we embark on this exciting and nerve-wracking experience, remember that everyone had to start somewhere. By embracing the relationship with your mentor you will learn to focus on your goals and help you to see how the subjects you are studying now will help your career in the future.  It is also an incredible opportunity to extend your professional
    network and learn to engage with established members of the tech community.
   </p>
-</Jumbotron>
-    </div>
+  <img src = {logo} style = {{width:"20%", display: "right"}} /> 
+</div>
+</div>
+
   );
 }
 
